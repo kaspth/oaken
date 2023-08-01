951fe0db82acaf5c6856d4bea31985b082929a99
Kasper Timm Hansen <kaspth@gmail.com>
2023-08-01 19:53:10 +0200

    Make the hacks come together for one last job

--- lib/oaken.rb
+++ lib/oaken.rb
@@ -6,22 +6,34 @@ module Oaken
   class Error < StandardError; end
 
   module Tables
+    extend self
+
     def users
-      Table.new(:users)
+      @@users ||= Table.new(:users)
+    end
+
+    def self.seeds(&block)
+      module_eval(&block)
     end
   end
 
   class Table
     def initialize(name)
       @name = name
+      @fixtures = {}
     end
 
     def update(name, **attributes)
-      if record = @records.find_by(id: name.hash)
-        record.update! **attributes
-      else
-        @records.create!(attributes)
-      end
+      require "ostruct" # TODO: Remove OpenStruct relatively soon.
+
+      @fixtures[name] = OpenStruct.new(attributes)
+      self.class.define_method(name) { @fixtures[name] }
+
+      # if record = @records.find_by(id: name.hash)
+      #   record.update! **attributes
+      # else
+      #   @records.create!(attributes)
+      # end
     end
   end
 end
--- test/test_helper.rb
+++ test/test_helper.rb
@@ -4,3 +4,11 @@ $LOAD_PATH.unshift File.expand_path("../lib", __dir__)
 require "oaken"
 
 require "minitest/autorun"
+
+class Oaken::Test < Minitest::Test
+  include Oaken::Tables
+end
+
+Oaken::Tables.seeds do
+  users.update :kasper, name: "Kasper"
+end

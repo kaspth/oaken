c65f6a8477f2d568e591b5ba2dad89456da058e3
Kasper Timm Hansen <kaspth@gmail.com>
2023-08-03 23:17:13 +0200

    we build this gem on a pile of hacks 🎶 (#2)
    * Commit sketch
    
    * Make the hacks come together for one last job

--- lib/oaken.rb
+++ lib/oaken.rb
@@ -4,5 +4,36 @@ require_relative "oaken/version"
 
 module Oaken
   class Error < StandardError; end
-  # Your code goes here...
+
+  module Tables
+    extend self
+
+    def users
+      @@users ||= Table.new(:users)
+    end
+
+    def self.seeds(&block)
+      module_eval(&block)
+    end
+  end
+
+  class Table
+    def initialize(name)
+      @name = name
+      @fixtures = {}
+    end
+
+    def update(name, **attributes)
+      require "ostruct" # TODO: Remove OpenStruct relatively soon.
+
+      @fixtures[name] = OpenStruct.new(attributes)
+      self.class.define_method(name) { @fixtures[name] }
+
+      # if record = @records.find_by(id: name.hash)
+      #   record.update! **attributes
+      # else
+      #   @records.create!(attributes)
+      # end
+    end
+  end
 end
--- test/test_oaken.rb
+++ test/test_oaken.rb
@@ -2,8 +2,19 @@
 
 require "test_helper"
 
-class TestOaken < Minitest::Test
+class TestOaken < Oaken::Test
   def test_that_it_has_a_version_number
     refute_nil ::Oaken::VERSION
+  end
+
+  def test_accessing_fixture
+    assert_equal "Kasper", users.kasper.name
+  end
+
+  def test_updating_fixture
+    users.update :kasper, name: "Kasper2"
+    assert_equal "Kasper2", users.kasper.name
+
+    users.update :kasper, name: "Kasper" # TODO: Hack to pretend we've got transactional tests working. Remove after we've connected Active Record.
   end
 end

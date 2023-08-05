02bb5b1851ba95facb056f5131697d6493e63c29
Kasper Timm Hansen <kaspth@gmail.com>
2023-08-05 19:52:46 +0200

    Modeling updates: rename classes + fleshing out Active Record access (#7)
    * Rename Tables to Data
    
    I'm trying to model this so we also support in-memory objects, so Tables is misleading now.
    
    Let's go with the more general purpose `Data`.
    
    I also considered `Readers` this module is chiefly about exposing the readers/accessors for each registered model.
    
    * Chop `seeds` API for now
    
    We want to start loading our data from files pretty soon and I'm not sure we want this block version really.
    
    * Rename Table to Stored::Memory
    
    We don't want just to support tables from a database, but start allowing in-memory objects too. This gets closer to that.
    
    * Rename the internal storage variable to objects; closer with where we're going
    
    * Provide barebones register API so we can start moving the type to the edge
    
    * Inject the type from the callsite; lets us remove OpenStruct too
    
    * This will be  pretty soon
    
    * Flesh out the potential first Active Record lookup sketch some more
    
    * Try sketching out an Abstract class for stored handlers
    
    * Clarify our two `name` variables
    
    One is really the overall key that we're using in the data table (and maybe our Stored classes shouldn't know about that at all),
    the other is more closely the id as a symbol.
    
    * Try removing the knowledge of our key from the storage for now
    
    * We will be needing the id here even though this has no tests yet
    
    * Add a providers seam to register on a specific provider
    
    * Preserve Ruby 3.0.0 compatibility

--- lib/oaken.rb
+++ lib/oaken.rb
@@ -5,35 +5,63 @@ require_relative "oaken/version"
 module Oaken
   class Error < StandardError; end
 
-  module Tables
-    extend self
-
-    def users
-      @@users ||= Table.new(:users)
+  module Stored; end
+  class Stored::Abstract
+    def initialize(type)
+      @type = type
     end
 
-    def self.seeds(&block)
-      module_eval(&block)
+    def update(id, **attributes)
+      self.class.define_method(id) { find(id) }
     end
   end
 
-  class Table
-    def initialize(name)
-      @name = name
-      @fixtures = {}
+  class Stored::Memory < Stored::Abstract
+    def find(id)
+      objects.fetch(id)
     end
 
-    def update(name, **attributes)
-      require "ostruct" # TODO: Remove OpenStruct relatively soon.
-
-      @fixtures[name] = OpenStruct.new(attributes)
-      self.class.define_method(name) { @fixtures[name] }
-
-      # if record = @records.find_by(id: name.hash)
-      #   record.update! **attributes
-      # else
-      #   @records.create!(attributes)
-      # end
+    def update(id, **attributes)
+      super
+      objects[id] = @type.new(**attributes)
     end
+
+    private def objects
+      @objects ||= {}
+    end
+  end
+
+  class Stored::ActiveRecord < Stored::Abstract
+    def find(id)
+      @type.find(id.hash)
+    end
+
+    def update(id, **attributes)
+      super
+
+      if record = @type.find_by(id: id.hash)
+        record.update!(**attributes)
+      else
+        @type.create!(id: id.hash, **attributes)
+      end
+    end
+  end
+
+  module Data
+    extend self
+
+    class Provider < Struct.new(:data, :provider)
+      def register(key, type)
+        stored = provider.new(type) and data.define_method(key) { stored }
+      end
+    end
+
+    def self.provider(name, provider)
+      define_singleton_method(name) { (@providers ||= {})[name] ||= Provider.new(self, provider) }
+      class_eval "def #{name}; self.class.#{name}; end"
+    end
+
+    provider :memory, Stored::Memory
+    provider :records, Stored::ActiveRecord
   end
 end
--- test/test_helper.rb
+++ test/test_helper.rb
@@ -6,9 +6,13 @@ require "oaken"
 require "minitest/autorun"
 
 class Oaken::Test < Minitest::Test
-  include Oaken::Tables
+  include Oaken::Data
 end
 
-Oaken::Tables.seeds do
-  users.update :kasper, name: "Kasper"
-end
+User = Struct.new(:name, keyword_init: true)
+Oaken::Data.memory.register :users, User
+
+Comment = Struct.new(:name, keyword_init: true)
+Oaken::Data.records.register :comments, Comment
+
+Oaken::Data.users.update :kasper, name: "Kasper"

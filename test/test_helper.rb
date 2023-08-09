7f028c0e7c2c0c22b28cfea46745b421ecd50c2e
Kasper Timm Hansen <kaspth@gmail.com>
2023-08-09 19:07:30 +0200

    Encourage combining certain fixtures into one file
    Part of goals is to start having people think of their models more from the object graph level.
    In other words, how things connect and not what models they are.
    
    Rails' standard fixtures makes that difficult because you have to traverse several files to build up those
    relationships.
    
    It would be better if we could group them in one file. So this starts that by combining our fixtures into one file.

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -9,4 +9,7 @@ class Oaken::Test < Minitest::Test
   include Oaken::Data
 end
 
+User = Struct.new(:name, keyword_init: true)
+Comment = Struct.new(:name, keyword_init: true)
+
 Oaken::Data.load_from "test/seeds"

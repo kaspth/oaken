b56024cc95ee9d3d16aa35fffabd66f6dff8475d
Kasper Timm Hansen <kaspth@gmail.com>
2023-08-22 20:13:19 +0200

    Use Pathnames throughout; use create_file so we get Rails' diffing

--- lib/generators/oaken/convert/fixtures_generator.rb
+++ lib/generators/oaken/convert/fixtures_generator.rb
@@ -3,39 +3,30 @@ require "yaml"
 
 module Oaken::Convert; end
 class Oaken::Convert::FixturesGenerator < Rails::Generators::Base
+  desc "Converts Rails fixtures to Oaken seeds"
   source_root File.expand_path("templates", __dir__)
 
-  desc "This generator converts Rails fixtures to seedable models"
+  def convert
+    path = Pathname.new("test/fixtures")
 
-  def convert_fixtures_to_seeds
-    fixtures_path = Rails.root.join("test/fixtures")
-    seeds_path = Rails.root.join("test/seeds")
+    Pathname.glob("**/*.{yml,yaml}", base: path) do |fixture_file|
+      output_file = Pathname.new("test/seeds") / fixture_file.sub_ext(".rb")
 
-    Dir.glob("#{fixtures_path}/**/*.{yml,yaml}") do |fixture_file|
-      begin
-        relative_path = Pathname.new(fixture_file).relative_path_from(fixtures_path)
-        output_file = seeds_path.join(relative_path.sub_ext(".rb"))
-
-        FileUtils.mkdir_p(output_file.dirname)
-
-        parsed_data = parse_fixture_file fixture_file
-        unless parsed_data
-          say "Skipped empty #{fixture_file}", :yellow
-          next
-        end
-
-        output = parsed_data.map do |key, attributes|
-          model_name = File.basename(relative_path, ".*")
-          attribute_strings = attributes.map { |k, v| "#{k}: #{recursive_convert(v)}" }.join(", ")
-          "#{model_name}.update :#{key}, #{attribute_strings}"
-        end
-
-        output_file.write output.join("\n")
-
-        say "Converted #{fixture_file} to #{output_file}", :green
-      rescue Psych::SyntaxError
-        say "Skipped #{fixture_file} due to ERB content or other YAML parsing issues.", :yellow
+      parsed_data = parse_fixture_file path / fixture_file
+      unless parsed_data
+        say "Skipped empty #{fixture_file}", :yellow
+        next
       end
+
+      output = parsed_data.map do |key, attributes|
+        model_name = output_file.basename(".*")
+        attribute_strings = attributes.map { |k, v| "#{k}: #{recursive_convert(v)}" }.join(", ")
+        "#{model_name}.update :#{key}, #{attribute_strings}"
+      end
+
+      create_file output_file, output.join("\n")
+    rescue Psych::SyntaxError
+      say "Skipped #{fixture_file} due to ERB content or other YAML parsing issues.", :yellow
     end
   end
 

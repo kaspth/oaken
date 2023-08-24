e5e6384345649a8aa008cb2881176cf31a136d4b
Kasper Timm Hansen <kaspth@gmail.com>
2023-08-24 19:48:06 +0200

    Support model namespaces
    If we're converting `post/comments.yml` we'll automatically insert a `register Post::Comment` at the top of the file.
    
    This also starts our namespace support to look like regular fixtures accessors, which will also use `post_comments` for namespaced models. We may improve on this later.
    
    We may figure out some better auto-resolving later, like just before we exec the file in `load_from`.

--- lib/generators/oaken/convert/fixtures_generator.rb
+++ lib/generators/oaken/convert/fixtures_generator.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 require "rails/generators/base"
 require "yaml"
 
@@ -10,19 +12,25 @@ class Oaken::Convert::FixturesGenerator < Rails::Generators::Base
     Pathname.glob("test/fixtures/**/*.{yml,yaml}") do |file|
       yaml = YAML.load_file(file)
 
-      model_name  = file.relative_path_from("test/fixtures").sub_ext ""
+      model_path  = file.relative_path_from("test/fixtures").sub_ext("").to_s
       output_file = file.sub("fixtures", "seeds").sub_ext(".rb")
-      create_file output_file, convert_one(model_name, yaml) || ""
+      create_file output_file, convert_one(model_path, yaml) || ""
     rescue Psych::SyntaxError
       say "Skipped #{fixture_file} due to ERB content or other YAML parsing issues.", :yellow
     end
   end
 
   private
-    def convert_one(model_name, contents)
-      contents.map do |key, attributes|
-        "#{model_name}.update :#{key}, #{recursive_convert(attributes, wrap: false)}"
-      end.join("\n") if contents
+    def convert_one(model_path, contents)
+      if contents
+        model_name = model_path.tr("/", "_")
+
+        contents.map do |key, attributes|
+          "#{model_name}.update :#{key}, #{recursive_convert(attributes, wrap: false)}"
+        end.join("\n").tap do
+          _1.prepend "register #{model_path.classify}\n" if model_path.include?("/")
+        end
+      end
     end
 
     def recursive_convert(input, wrap: true)

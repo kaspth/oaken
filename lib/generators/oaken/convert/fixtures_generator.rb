d3b2a4d859861254cd4eb7ed078f7fc7298bbfb9
Kasper Timm Hansen <kaspth@gmail.com>
2023-08-24 19:58:47 +0200

    Converter: refactor + support diffing & namespaced models (#27)
    * Rename converter command to oaken:convert:fixtures
    
    Updates the documentation reflect this too.
    
    * Standardize on double-quoted strings
    
    * Trim empty file language a bit
    
    * We can use map here
    
    * Use Pathnames throughout; use create_file so we get Rails' diffing
    
    * Create empty files to mirror the empty yml file
    
    * Prefer a case statement to wrapping is_a? checks
    
    * See what happens if we cut down inner loop
    
    * Cleaner? hm
    
    * Support model namespaces
    
    If we're converting `post/comments.yml` we'll automatically insert a `register Post::Comment` at the top of the file.
    
    This also starts our namespace support to look like regular fixtures accessors, which will also use `post_comments` for namespaced models. We may improve on this later.
    
    We may figure out some better auto-resolving later, like just before we exec the file in `load_from`.
    
    * Extract helepr method to avoid boolean argument
    
    * Remove unused param

---
+++ lib/generators/oaken/convert/fixtures_generator.rb
@@ -0,0 +1,48 @@
+# frozen_string_literal: true
+
+require "rails/generators/base"
+require "yaml"
+
+module Oaken::Convert; end
+class Oaken::Convert::FixturesGenerator < Rails::Generators::Base
+  desc "Converts Rails fixtures to Oaken seeds"
+  source_root File.expand_path("templates", __dir__)
+
+  def convert_all
+    Pathname.glob("test/fixtures/**/*.{yml,yaml}") do |file|
+      yaml = YAML.load_file(file)
+
+      model_path  = file.relative_path_from("test/fixtures").sub_ext("").to_s
+      output_file = file.sub("fixtures", "seeds").sub_ext(".rb")
+      create_file output_file, convert_one(model_path, yaml) || ""
+    rescue Psych::SyntaxError
+      say "Skipped #{fixture_file} due to ERB content or other YAML parsing issues.", :yellow
+    end
+  end
+
+  private
+    def convert_one(model_path, contents)
+      if contents
+        model_name = model_path.tr("/", "_")
+
+        contents.map do |key, attributes|
+          "#{model_name}.update :#{key}, #{convert_hash(attributes)}"
+        end.join("\n").tap do
+          _1.prepend "register #{model_path.classify}\n" if model_path.include?("/")
+        end
+      end
+    end
+
+    def recursive_convert(input)
+      case input
+      when Hash  then "{ #{convert_hash(input)} }"
+      when Array then input.map { recursive_convert _1 }.join(", ")
+      else
+        "\"#{input}\""
+      end
+    end
+
+    def convert_hash(hash)
+      hash.map { |k, v| "#{k}: #{recursive_convert(v)}" }.join(", ")
+    end
+end

385b540e554e216745a43b5cea92a8355b19c63b
Kasper Timm Hansen <kaspth@gmail.com>
2023-10-18 00:50:28 +0200

    Graduate Entry into its own file

--- lib/oaken.rb
+++ lib/oaken.rb
@@ -3,10 +3,11 @@
 require "oaken/version"
 require "pathname"
 require "yaml"
-require "digest/md5"
 
 module Oaken
   class Error < StandardError; end
+
+  autoload :Entry, "oaken/entry"
 
   class Inflector
     def tableize(string)
@@ -97,61 +98,6 @@ module Oaken
         @entry = entry
         @entry.load_onto seeds
       end
-    end
-  end
-
-  require "pstore"
-
-  class Entry < DelegateClass(PStore)
-    def self.store_accessor(name)
-      define_method(name) { self[name] } and define_method("#{name}=") { |value| self[name] = value }
-    end
-    store_accessor :checksum
-    store_accessor :readers
-
-    def self.within(directory)
-      Pathname.glob("#{directory}{,/**/*}.rb").sort.map { new _1 }
-    end
-
-    def initialize(pathname)
-      @file, @pathname = pathname.to_s, pathname
-      @computed_checksum = Digest::MD5.hexdigest(@pathname.read)
-
-      prepared_store_path = Oaken.store_path.join(pathname).tap { _1.dirname.mkpath }
-      super PStore.new(prepared_store_path)
-    end
-
-    def load_onto(seeds)
-      transaction do
-        if replay?
-          puts "Replaying #{@file}…"
-          readers.each do |key, name, id, lineno|
-            seeds.send(key).instance_eval "def #{name}; find #{id}; end", @file, lineno
-          end
-        else
-          reset
-          seeds.class_eval @pathname.read, @file
-        end
-      end
-    end
-
-    def replay?
-      checksum == @computed_checksum
-    end
-
-    def reset
-      self.checksum = @computed_checksum
-      self.readers  = Set.new
-    end
-
-    def define_reader(stored, name, id)
-      lineno = self.lineno
-      stored.instance_eval "def #{name}; find #{id}; end", @file, lineno
-      readers << [stored.key, name, id, lineno]
-    end
-
-    def lineno
-      caller_locations(3, 10).find { _1.path == @file }.lineno
     end
   end
 end

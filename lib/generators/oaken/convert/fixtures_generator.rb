b5393f3801b5e2cf3b81d0e6720bb6dd0c0b4c08
Kasper Timm Hansen <kaspth@gmail.com>
2023-11-02 22:21:34 +0100

    Move Fixture out of generator namespace

--- lib/generators/oaken/convert/fixtures_generator.rb
+++ lib/generators/oaken/convert/fixtures_generator.rb
@@ -4,6 +4,70 @@ require "rails/generators/base"
 require "yaml"
 
 module Oaken::Convert; end
+class Oaken::Convert::Fixture
+  attr_reader :model_name, :name
+
+  def initialize(model_name, name, attributes)
+    @model_name, @name, @attributes = model_name.tr("/", "_"), name, attributes
+    @plural = @model_name
+    @singular = @model_name.singularize
+  end
+
+  def self.traverse(fixtures, root_model_name)
+    fixtures = fixtures.to_h do |model_name, rows|
+      [model_name, rows.map { new(model_name, _1, _2) }]
+    end
+
+    roots = fixtures.delete(root_model_name)
+    fixtures = fixtures.values.flatten
+
+    roots.each do |fixture|
+      fixture.extract_dependents fixtures
+    end
+  end
+
+  def extract_dependents(fixtures)
+    @dependents = fixtures.select { _1.reference(plural, singular) == name }
+    fixtures.replace fixtures - dependents
+
+    dependents.each { _1.extract_dependents fixtures }
+  end
+
+  def reference(plural, singular)
+    @referenced = plural   if attributes[plural]
+    @referenced = singular if attributes[singular]
+    attributes[@referenced]
+  end
+
+  def render(delimiter: "\n")
+    [render_self, dependents.map { _1.render delimiter: nil }].join(delimiter)
+  end
+
+  private
+    attr_reader :attributes, :dependents
+    attr_reader :plural, :singular
+
+    def render_self
+      "#{model_name}.create :#{name}, #{convert_hash(attributes)}\n".tap do
+        _1.prepend "#{name} = " if dependents.any?
+      end
+    end
+
+    def convert_hash(hash)
+      hash.map { |k, v| "#{k}: #{recursive_convert(v, key: k)}" }.join(", ")
+    end
+
+    def recursive_convert(input, key: nil)
+      case input
+      when Hash  then "{ #{convert_hash(input)} }"
+      when Array then input.map { recursive_convert _1 }.join(", ")
+      when Integer then input
+      else
+        key == @referenced ? input : "\"#{input}\""
+      end
+    end
+end
+
 class Oaken::Convert::FixturesGenerator < Rails::Generators::Base
   desc "Converts fixtures to Oaken seeds in db/seeds/test"
   source_root File.expand_path("templates", __dir__)
@@ -26,7 +90,7 @@ class Oaken::Convert::FixturesGenerator < Rails::Generators::Base
   end
 
   def convert_all
-    Fixture.traverse(@fixtures, @root_model.collection).each do |fixture|
+    Oaken::Convert::Fixture.traverse(@fixtures, @root_model.collection).each do |fixture|
       create_file "db/seeds/test/#{@root_model.plural}/#{fixture.name}.rb", fixture.render
     end
   end
@@ -44,70 +108,6 @@ class Oaken::Convert::FixturesGenerator < Rails::Generators::Base
   end
 
   private
-    class Fixture
-      attr_reader :model_name, :name
-
-      def initialize(model_name, name, attributes)
-        @model_name, @name, @attributes = model_name.tr("/", "_"), name, attributes
-        @plural = @model_name
-        @singular = @model_name.singularize
-      end
-
-      def self.traverse(fixtures, root_model_name)
-        fixtures = fixtures.to_h do |model_name, rows|
-          [model_name, rows.map { Fixture.new(model_name, _1, _2) }]
-        end
-
-        roots = fixtures.delete(root_model_name)
-        fixtures = fixtures.values.flatten
-
-        roots.each do |fixture|
-          fixture.extract_dependents fixtures
-        end
-      end
-
-      def extract_dependents(fixtures)
-        @dependents = fixtures.select { _1.reference(plural, singular) == name }
-        fixtures.replace fixtures - dependents
-
-        dependents.each { _1.extract_dependents fixtures }
-      end
-
-      def reference(plural, singular)
-        @referenced = plural   if attributes[plural]
-        @referenced = singular if attributes[singular]
-        attributes[@referenced]
-      end
-
-      def render(delimiter: "\n")
-        [render_self, dependents.map { _1.render delimiter: nil }].join(delimiter)
-      end
-
-      private
-        attr_reader :attributes, :dependents
-        attr_reader :plural, :singular
-
-        def render_self
-          "#{model_name}.create :#{name}, #{convert_hash(attributes)}\n".tap do
-            _1.prepend "#{name} = " if dependents.any?
-          end
-        end
-
-        def convert_hash(hash)
-          hash.map { |k, v| "#{k}: #{recursive_convert(v, key: k)}" }.join(", ")
-        end
-
-        def recursive_convert(input, key: nil)
-          case input
-          when Hash  then "{ #{convert_hash(input)} }"
-          when Array then input.map { recursive_convert _1 }.join(", ")
-          when Integer then input
-          else
-            key == @referenced ? input : "\"#{input}\""
-          end
-        end
-    end
-
     def empty_directory_with_keep_file(name)
       empty_directory name
       create_file "#{name}/.keep" if options[:keeps]

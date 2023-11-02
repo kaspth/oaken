a5f8e5ffcebb39717dc62beb52663ab5b297eddf
Kasper Timm Hansen <kaspth@gmail.com>
2023-11-02 23:29:40 +0100

    Group Fixtures converter output around root model e.g. Account (#65)
    * Start updating fixtures conversion; we'll try to group around root model like Accounts
    
    * Prep a lot more of the surrounding scaffolding
    
    * I don't remember what my idea was with this root_model accessor
    
    * Almost got the first level of the grouping on the root model happening
    
    * Getting closer?
    
    * Move up file path processing
    
    * better?
    
    * We've done this processing already now
    
    * Fix namespaced models registering syntax
    
    * uhhh more fixes and such
    
    * Split out descendants finding from processing
    
    * Start extracting details into a Fixture class
    
    * yooo, I think we're getting there
    
    * Track reference within a fixture
    
    * wow, we got it working recursively
    
    * Defer descendants until we're actually setting them
    
    * Space with two newlines at the top-level
    
    * oops, these aren't descendants they're dependents
    
    * Move Fixture out of generator namespace
    
    * Refactor to also build data files + fix final line endings in files
    
    * Fix empty spaces showing up when there's no register
    
    * Extract fixture conversion into our original loop
    
    * Fix generating a wrapping array when referenced in the plural form
    
    * Move this up and save an extra instance variable

--- lib/generators/oaken/convert/fixtures_generator.rb
+++ lib/generators/oaken/convert/fixtures_generator.rb
@@ -4,45 +4,111 @@ require "rails/generators/base"
 require "yaml"
 
 module Oaken::Convert; end
-class Oaken::Convert::FixturesGenerator < Rails::Generators::Base
-  desc "Converts Rails fixtures to Oaken seeds"
-  source_root File.expand_path("templates", __dir__)
+class Oaken::Convert::Fixture
+  attr_reader :model_name, :name
 
-  def convert_all
-    Pathname.glob("test/fixtures/**/*.{yml,yaml}") do |file|
-      yaml = YAML.load_file(file)
+  def initialize(model_name, name, attributes)
+    @model_name, @name, @attributes = model_name.tr("/", "_"), name, attributes
+    @plural = @model_name
+    @singular = @model_name.singularize
+  end
 
-      model_path  = file.relative_path_from("test/fixtures").sub_ext("").to_s
-      output_file = file.sub("fixtures", "seeds").sub_ext(".rb")
-      create_file output_file, convert_one(model_path, yaml) || ""
-    rescue Psych::SyntaxError
-      say "Skipped #{fixture_file} due to ERB content or other YAML parsing issues.", :yellow
-    end
+  def extract_dependents(fixtures)
+    @dependents = fixtures.select { _1.reference(plural, singular) == name }
+    fixtures.replace fixtures - dependents
+
+    dependents.each { _1.extract_dependents fixtures }
+  end
+
+  def reference(plural, singular)
+    @referenced = [plural, :plural]     if attributes[plural]
+    @referenced = [singular, :singular] if attributes[singular]
+    attributes[@referenced&.first]
+  end
+
+  def render(delimiter: "\n")
+    [render_self, dependents&.map { _1.render delimiter: nil }].join(delimiter)
   end
 
   private
-    def convert_one(model_path, contents)
-      if contents
-        model_name = model_path.tr("/", "_")
+    attr_reader :attributes, :dependents
+    attr_reader :plural, :singular
 
-        contents.map do |key, attributes|
-          "#{model_name}.update :#{key}, #{convert_hash(attributes)}"
-        end.join("\n").tap do
-          _1.prepend "register #{model_path.classify}\n" if model_path.include?("/")
-        end
-      end
-    end
-
-    def recursive_convert(input)
-      case input
-      when Hash  then "{ #{convert_hash(input)} }"
-      when Array then input.map { recursive_convert _1 }.join(", ")
-      else
-        "\"#{input}\""
+    def render_self
+      "#{model_name}.create :#{name}, #{convert_hash(attributes)}\n".tap do
+        _1.prepend "#{name} = " if dependents&.any?
       end
     end
 
     def convert_hash(hash)
-      hash.map { |k, v| "#{k}: #{recursive_convert(v)}" }.join(", ")
+      hash.map { |k, v| "#{k}: #{recursive_convert(v, key: k)}" }.join(", ")
+    end
+
+    def recursive_convert(input, key: nil)
+      case input
+      when Hash  then "{ #{convert_hash(input)} }"
+      when Array then input.map { recursive_convert _1 }.join(", ")
+      when Integer then input
+      else
+        if key == @referenced&.first
+          @referenced.last == :plural ? "[#{input}]" : input
+        else
+          "\"#{input}\""
+        end
+      end
+    end
+end
+
+class Oaken::Convert::FixturesGenerator < Rails::Generators::Base
+  desc "Converts fixtures to Oaken seeds in db/seeds/test"
+  source_root File.expand_path("templates", __dir__)
+
+  class_option :root_model, required: true
+  class_option :keeps, type: :boolean, default: true
+
+  def prepare
+    @root_model = ActiveModel::Name.new(options[:root_model].constantize)
+    empty_directory_with_keep_file "db/seeds/data"
+    empty_directory_with_keep_file "db/seeds/test/cases"
+  end
+
+  def parse
+    @fixtures = Dir.glob("test/fixtures/**/*.yml").to_h do |path|
+      model_name = path.delete_prefix("test/fixtures/").chomp(".yml")
+      [model_name, YAML.load_file(path)&.map { Oaken::Convert::Fixture.new(model_name, _1, _2) }]
+    rescue Psych::SyntaxError
+      say "Skipped #{path} due to ERB content or other YAML parsing issues.", :yellow
+    end.tap(&:compact_blank!)
+  end
+
+  def prepend_prepare_to_seeds
+    namespaces = @fixtures.keys.filter_map { _1.classify if _1.include?("/") }.uniq.sort
+
+    code  = +"Oaken.prepare do\n"
+    code << "  register #{namespaces.join(", ")}\n\n" if namespaces.any?
+    code << "  load :#{@root_model.plural}, :data\n"
+    code << "end\n"
+
+    inject_into_file "db/seeds.rb", code, before: /\A/
+  end
+
+  def convert_all
+    roots = @fixtures.delete(@root_model.collection)
+    @fixtures = @fixtures.values.flatten
+
+    roots.each do |fixture|
+      fixture.extract_dependents @fixtures
+      create_file "db/seeds/test/#{@root_model.plural}/#{fixture.name}.rb", fixture.render.chomp
+    end
+
+    @fixtures.group_by(&:model_name).each do |model_name, fixtures|
+      create_file "db/seeds/test/data/#{model_name}.rb", fixtures.map(&:render).join.chomp
+    end
+  end
+
+  private
+    def empty_directory_with_keep_file(name)
+      empty_directory name
+      create_file "#{name}/.keep" if options[:keeps]
     end
 end

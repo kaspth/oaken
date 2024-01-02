1a8907b8cb1b553b93fe1f8a7b60b20841948ea7
Kasper Timm Hansen <kaspth@gmail.com>
2024-01-02 19:06:27 +0100

    Single Quotes for SQL 🎶

--- test/dummy/db/seeds/test/cases/pagination.sql
+++ test/dummy/db/seeds/test/cases/pagination.sql
@@ -1,1 +1,1 @@
-INSERT INTO users (name, created_at, updated_at) VALUES ("pagination.sql", TIME("NOW"), TIME("NOW"))
+INSERT INTO users (name, created_at, updated_at) VALUES ('pagination.sql', TIME('NOW'), TIME('NOW'))

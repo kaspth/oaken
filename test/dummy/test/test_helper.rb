f7833ebd46635a41a3dd172316b57eba78a0722b
Kasper Timm Hansen <kaspth@gmail.com>
2024-01-07 20:18:37 +0100

    Run tests both parallelized and unparallelized

--- test/dummy/test/test_helper.rb
+++ test/dummy/test/test_helper.rb
@@ -5,7 +5,7 @@ require "rails/test_help"
 ActiveRecord::Base.logger = Logger.new(STDOUT)
 
 class ActiveSupport::TestCase
-  parallelize workers: :number_of_processors, threshold: 5
+  parallelize workers: :number_of_processors, threshold: ENV.fetch("PARALLEL_TEST_THRESHOLD", 5).to_i
 
   include Oaken::TestSetup
 end

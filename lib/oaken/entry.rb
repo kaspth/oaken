0f64019e6f2842eafcf9cf6b29c33a7c3c32567e
Kasper Timm Hansen <kaspth@gmail.com>
2024-01-07 19:07:09 +0100

    Start slimming down what's in an Entry

--- lib/oaken/entry.rb
+++ lib/oaken/entry.rb
@@ -1,29 +1,14 @@
-require "digest/md5"
-require "pstore"
-
-class Oaken::Entry < DelegateClass(PStore)
-  def self.store_accessor(name)
-    define_method(name) { self[name] } and define_method("#{name}=") { |value| self[name] = value }
-  end
-  store_accessor :checksum
-  store_accessor :readers
-
+class Oaken::Entry
   def self.within(directory)
     Pathname.glob("#{directory}{,/**/*}.{rb,sql}").sort.map { new _1 }
   end
 
   def initialize(pathname)
     @file, @pathname = pathname.to_s, pathname
-    @computed_checksum = Digest::MD5.hexdigest(@pathname.read)
-
-    prepared_store_path = Oaken.store_path.join(pathname).tap { _1.dirname.mkpath }
-    super PStore.new(prepared_store_path)
   end
 
   def load_onto(seeds)
-    transaction do
-      reset
-
+    Oaken.transaction do
       case @pathname.extname
       in ".rb"  then seeds.class_eval @pathname.read, @file
       in ".sql" then ActiveRecord::Base.connection.execute @pathname.read
@@ -31,23 +16,7 @@ class Oaken::Entry < DelegateClass(PStore)
     end
   end
 
-  def transaction(&block)
-    super do
-      Oaken.transaction(&block)
-    end
-  end
-
-  def replay?
-    checksum == @computed_checksum
-  end
-
-  def reset
-    self.checksum = @computed_checksum
-    self.readers  = Set.new
-  end
-
   def define_reader(stored, name, id, lineno)
     stored.instance_eval "def #{name}; find #{id}; end", @file, lineno
-    readers << [stored.key, name, id, lineno]
   end
 end

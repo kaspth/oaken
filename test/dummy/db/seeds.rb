112f3e7bbf62a6729e8dc2087de60764c5a686e7
Kasper Timm Hansen <kaspth@gmail.com>
2024-01-14 23:27:54 +0100

    Name ** so Ruby 3.0 works

--- test/dummy/db/seeds.rb
+++ test/dummy/db/seeds.rb
@@ -6,12 +6,12 @@ Oaken.prepare do
   user_counter, email_address_counter = 0, 0
   users.defaults name: -> { "Customer #{user_counter += 1}" },
     email_address: -> { "email_address#{email_address_counter += 1}@example.com" }
-  def users.create(*, unique_by: :email_address, **) = super
-  def users.create_labelled(label, **) = create(label, **)
+  def users.create(*, unique_by: :email_address, **o) = super
+  def users.create_labelled(label, **o) = create(label, **o)
 
   section :stems
   section :leafs
-  def plans.upsert(*, unique_by: :title, **) = super
+  def plans.upsert(*, unique_by: :title, **o) = super
 
   section do
     seed :accounts, :data

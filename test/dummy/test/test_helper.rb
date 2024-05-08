77f5b556a4e5b49837a37c89885156873ab1c236
Kasper Timm Hansen <kaspth@gmail.com>
2024-05-08 10:39:08 -0400

    Let's silence the test log output by default

--- test/dummy/test/test_helper.rb
+++ test/dummy/test/test_helper.rb
@@ -2,7 +2,7 @@ ENV["RAILS_ENV"] ||= "test"
 require_relative "../config/environment"
 require "rails/test_help"
 
-ActiveRecord::Base.logger = Logger.new(STDOUT)
+ActiveRecord::Base.logger = Logger.new(STDOUT) if ENV["VERBOSE"] || ENV["CI"]
 
 class ActiveSupport::TestCase
   parallelize workers: :number_of_processors, threshold: ENV.fetch("PARALLEL_TEST_THRESHOLD", 5).to_i

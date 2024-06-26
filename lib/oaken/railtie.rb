16adc11077016bd61a3daed27891b9c205d8da81
Thomas Cannon <tcannon00@gmail.com>
2024-06-26 07:52:46 -0400

    Update railtie to set Oaken as the fixture replacement
    * See:
    	* https://github.com/search?q=repo%3Arails%2Frails%20fixture_replacement&type=code
    	* https://github.com/search?q=repo%3Athoughtbot%2Ffactory_bot_rails%20fixture_replacement&type=code
    	* https://github.com/thoughtbot/factory_bot_rails#generators

--- lib/oaken/railtie.rb
+++ lib/oaken/railtie.rb
@@ -1,5 +1,23 @@
-class Oaken::Railtie < Rails::Railtie
-  initializer "oaken.lookup_paths" do
-    Oaken.lookup_paths << "db/seeds/#{Rails.env}"
+module Oaken
+  class Railtie < Rails::Railtie
+    initializer "oaken.set_fixture_replacement" do
+      GeneratorConfiguration.run(config)
+    end
+
+    initializer "oaken.lookup_paths" do
+      Oaken.lookup_paths << "db/seeds/#{Rails.env}"
+    end
+  end
+
+  class GeneratorConfiguration
+    def self.run(config)
+      test_framework = config.app_generators.options[:rails][:test_framework]
+
+      config.app_generators.test_framework(
+        test_framework,
+        fixture: false,
+        fixture_replacement: :oaken
+      )
+    end
   end
 end

1396de7128b4f78c77ae97be387d790212757607
Kasper Timm Hansen <kaspth@gmail.com>
2024-06-28 16:53:20 +0200

    Disable Fixtures when using Oaken.
    We don't have a fixture_replacement, so I don't think we should set that.
    
    I did some digging in the code and this invocation is the same as the previous commit when running with Rails' default test setup.

--- lib/oaken/railtie.rb
+++ lib/oaken/railtie.rb
@@ -1,23 +1,9 @@
 module Oaken
   class Railtie < Rails::Railtie
-    initializer "oaken.set_fixture_replacement" do
-      GeneratorConfiguration.run(config)
-    end
+    config.app_generators.test_unit fixture: false
 
     initializer "oaken.lookup_paths" do
       Oaken.lookup_paths << "db/seeds/#{Rails.env}"
-    end
-  end
-
-  class GeneratorConfiguration
-    def self.run(config)
-      test_framework = config.app_generators.options[:rails][:test_framework]
-
-      config.app_generators.test_framework(
-        test_framework,
-        fixture: false,
-        fixture_replacement: :oaken
-      )
     end
   end
 end

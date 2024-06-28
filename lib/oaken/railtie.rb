41be2a593d8326c6229431833b6cda2101fff6e1
Kasper Timm Hansen <kaspth@gmail.com>
2024-06-28 17:10:37 +0200

    Slight revert: hopefully handle RSpec with this version too

--- lib/oaken/railtie.rb
+++ lib/oaken/railtie.rb
@@ -1,6 +1,8 @@
 module Oaken
   class Railtie < Rails::Railtie
-    config.app_generators.test_unit fixture: false
+    config.app_generators do
+      _1.test_framework _1.test_framework, fixture: false
+    end
 
     initializer "oaken.lookup_paths" do
       Oaken.lookup_paths << "db/seeds/#{Rails.env}"

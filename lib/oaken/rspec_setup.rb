3daac04049ebb8b80ca6950c232d2b24d2148605
Kasper Timm Hansen <kaspth@gmail.com>
2024-06-30 19:45:24 +0200

    Document RSpec setup (#84)
    * Document RSpec setup
    
    You need to require this one file.
    
    * Just a lil stylin'

--- lib/oaken/rspec_setup.rb
+++ lib/oaken/rspec_setup.rb
@@ -1,9 +1,8 @@
 RSpec.configure do |config|
-  config.include(Oaken::Seeds)
-
+  config.include Oaken::Seeds
   config.use_transactional_fixtures = true
 
-  config.before(:suite) do
+  config.before :suite do
     # Mimic fixtures by truncating before inserting.
     ActiveRecord::Tasks::DatabaseTasks.truncate_all
     Oaken.load_seed

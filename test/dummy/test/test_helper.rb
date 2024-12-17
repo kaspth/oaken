98c517d93f1729d05f955fcd12a64f4da83469ea
Kasper Timm Hansen <hey@kaspth.com>
2024-12-18 00:51:12 +0100

    This isn't worth it unfortunately

--- test/dummy/test/test_helper.rb
+++ test/dummy/test/test_helper.rb
@@ -8,10 +8,4 @@ class ActiveSupport::TestCase
   parallelize workers: :number_of_processors, threshold: ENV.fetch("PARALLEL_TEST_THRESHOLD", 5).to_i
 
   include Oaken::TestSetup
-
-  setup do
-    db = ActiveRecord::Base.connection.raw_connection
-    db.enable_load_extension(true)
-    SqliteUlid.load db
-  end if ENV["RAILS_VERSION"] == "7.1"
 end

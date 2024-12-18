9234cd26194470c4c8fce95d7cf5a6c0a812f82a
Kasper Timm Hansen <hey@kaspth.com>
2024-12-18 01:14:21 +0100

    Document how to disable Fixtures (#92)
    * See:
    	* https://github.com/search?q=repo%3Arails%2Frails%20fixture_replacement&type=code
    	* https://github.com/search?q=repo%3Athoughtbot%2Ffactory_bot_rails%20fixture_replacement&type=code
    	* https://github.com/thoughtbot/factory_bot_rails#generators
    
    ---------
    
    Co-authored-by: Thomas Cannon <tcannon00@gmail.com>

--- lib/oaken/railtie.rb
+++ lib/oaken/railtie.rb
@@ -1,5 +1,7 @@
-class Oaken::Railtie < Rails::Railtie
-  initializer "oaken.lookup_paths" do
-    Oaken.lookup_paths << "db/seeds/#{Rails.env}"
+module Oaken
+  class Railtie < Rails::Railtie
+    initializer "oaken.lookup_paths" do
+      Oaken.lookup_paths << "db/seeds/#{Rails.env}"
+    end
   end
 end
--- test/dummy/config/application.rb
+++ test/dummy/config/application.rb
@@ -18,5 +18,7 @@ module Dummy
     #
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
+
+    config.app_generators { _1.test_framework _1.test_framework, fixture: false }
   end
 end

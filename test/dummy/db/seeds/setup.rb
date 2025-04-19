8472e448dfd9062d6caae7b5a52cbb22b32c86c5
Kasper Timm Hansen <hey@kaspth.com>
2025-04-19 16:15:10 +0200

    Calling this on the loader is more appropriate

--- test/dummy/db/seeds/setup.rb
+++ test/dummy/db/seeds/setup.rb
@@ -1,4 +1,4 @@
-Oaken.defaults name: -> { "Shouldn't be used for users.name" }, title: -> { "Global Default Title" }
+loader.defaults name: -> { "Shouldn't be used for users.name" }, title: -> { "Global Default Title" }
 
 user_counter, email_address_counter = 0, 0
 users.defaults name: -> { "Customer #{user_counter += 1}" },

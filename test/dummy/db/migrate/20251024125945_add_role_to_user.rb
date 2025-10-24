957fe877e78f42eee12d97eebe5ada26db6e823a
Kasper Timm Hansen <hey@kaspth.com>
2025-10-24 15:12:02 +0200

    Use earliest Rails version we support

--- test/dummy/db/migrate/20251024125945_add_role_to_user.rb
+++ test/dummy/db/migrate/20251024125945_add_role_to_user.rb
@@ -1,4 +1,4 @@
-class AddRoleToUser < ActiveRecord::Migration[8.0]
+class AddRoleToUser < ActiveRecord::Migration[7.2]
   def change
     add_column :users, :role, :string, null: false, default: "plain"
   end
